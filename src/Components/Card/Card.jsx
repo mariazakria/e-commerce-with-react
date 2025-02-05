a658435b1a924f0e8f3966e8fd99ea592122e2a1
mariazakria <marizakria130@gmail.com>
2025-02-05 06:27:53 +0200

    edit navbar

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,14 +8,14 @@ export default function Card({ productInfo }) {
       <div className="mb-8 card group rounded-lg shadow-lg overflow-hidden cursor-pointer transition-transform duration-300  hover:scale-100  hover:border hover:border-primary-600">
         <div className="relative">
           <img src={imageCover} alt={title} className="card-img" />
-          {/* <div className="layer group-hover:opacity-100 transition-opacity duration-300 gap-2 flex justify-center items-center absolute w-full h-full left-0 top-0 bg-slate-400 bg-opacity-35 opacity-0">
+          <div className="layer group-hover:opacity-100 transition-opacity duration-300 gap-2 flex justify-center items-center absolute w-full h-full left-0 top-0 bg-slate-400 bg-opacity-35 opacity-0">
             <div className="icon cursor-pointer w-8 h-8 rounded-full bg-primary-500 text-white flex justify-center items-center">
               <i className="fa-solid fa-eye"></i>
             </div>
             <div className="icon cursor-pointer w-8 h-8 rounded-full bg-primary-500 text-white flex justify-center items-center">
-              <i className="fa-solid fa-cart-shopping"></i>
+              <i className="fa-solid fa-heart"></i>
             </div>
-          </div> */}
+          </div>
         </div>
 
         <div className="space-y-3 card-body p-4">
@@ -24,12 +24,10 @@ export default function Card({ productInfo }) {
             <h4 className="text-primary-500 font-semibold">{category.name}</h4>
           </header>
           <p className="text-gray-400 line-clamp-2 text-sm">{description}</p>
-          <span className="text-primary-800 font-semibold">{price} L.E</span>
 
           <div className="flex items-center justify-between">
-            <div className="icon cursor-pointer w-8 h-8 rounded-full bg-primary-500 text-white flex justify-center items-center">
-              <i className="fa-solid fa-heart"></i>
-            </div>
+          <span className="text-primary-800 font-semibold">{price} L.E</span>
+
             <div className="flex items-center space-x-1">
               <span>{ratingsAverage}</span>
               <i className="fa-solid fa-star text-yellow-300"></i>
